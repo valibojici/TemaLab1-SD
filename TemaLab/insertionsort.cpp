a30ade08d13f95590d922d8f74755c57d930a750
Vali Bojici <valibojici@gmail.com>
2021-02-21 20:40:29 +0200

    change int to size_t

--- TemaLab/insertionsort.cpp
+++ TemaLab/insertionsort.cpp
@@ -6,7 +6,7 @@ void insertion_sort(std::vector<ULL>& nums, size_t start, size_t end)
 	for (unsigned int i = start+1; i <= end; ++i)
 	{
 		ULL x = nums[i];
-		int j = i;
+		size_t j = i;
 		while (j > 0 && nums[j-1] > x)
 		{
 			nums[j] = nums[j-1];
