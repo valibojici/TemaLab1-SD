da430c4d0526413d949d129f1c597147a445ec62
Vali Bojici <valibojici@gmail.com>
2021-02-21 12:07:55 +0200

    added insertion

---
+++ TemaLab/insertionsort.cpp
@@ -0,0 +1,17 @@
+#include "insertionsort.h"
+
+typedef unsigned long long ULL;
+void insertion_sort(std::vector<ULL>& nums, size_t start, size_t end)
+{
+	for (unsigned int i = start+1; i <= end; ++i)
+	{
+		ULL x = nums[i];
+		int j = i;
+		while (j > 0 && nums[j-1] > x)
+		{
+			nums[j] = nums[j-1];
+			j--;
+		}
+		nums[j] = x;
+	}
+}
---
+++ TemaLab/insertionsort.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+
+
+void insertion_sort(std::vector<unsigned long long>&,size_t,size_t );
