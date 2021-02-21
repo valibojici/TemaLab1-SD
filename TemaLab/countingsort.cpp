e8fd2e6b638d62a3cd5d84a8826275cce0401d96
Vali Bojici <valibojici@gmail.com>
2021-02-21 12:08:12 +0200

    update

--- TemaLab/countingsort.cpp
+++ TemaLab/countingsort.cpp
@@ -1,24 +1,37 @@
 #include "countingsort.h"
+#include <iostream>
 
 typedef unsigned long long ULL;
 
-bool counting_sort(std::vector<ULL>& nums)
+bool check_memory(const std::vector<ULL>& nums)
 {
 	ULL mini = nums[0], maxi = nums[0];
-	for (unsigned int i = 1; i < nums.size(); ++i)
+	for (int i = 1; i < nums.size(); ++i)
+		mini = std::min(mini, nums[i]), maxi = std::max(maxi, nums[i]);
+	
+	if (maxi - mini + 1 > 1000000000)
+		return false;
+	return true;
+}
+
+
+void counting_sort(std::vector<ULL>& nums,size_t start, size_t end)
+{
+	ULL mini = nums[start], maxi = nums[start];
+	for (int i = start + 1; i <= end; ++i)
 		mini = std::min(mini, nums[i]), maxi = std::max(maxi, nums[i]);
 
 	// daca sunt prea multe numere depasesc memoria
-	if (maxi - mini + 1 > 1500000000)return false;
+	if (maxi - mini + 1 > 1500000000) {
+		return;
+	}
 
-	std::vector<unsigned int> count(maxi - mini + 1, 0);
-	for (unsigned int i = 0; i < nums.size(); ++i)
+	std::vector<int> count(maxi - mini + 1, 0);
+	for (int i = start; i <= end; ++i)
 		count[nums[i] - mini]++;
 
-	unsigned int poz = 0;
-	for (unsigned int i = 0; i < maxi - mini + 1; ++i)
-		for (unsigned int j = 0; j < count[i]; ++j)
+	int poz = start;
+	for (int i = 0; i < maxi - mini + 1; ++i)
+		for (int j = 0; j < count[i]; ++j)
 			nums[poz++] = i + mini;
-
-	return true;
 }
--- TemaLab/countingsort.h
+++ TemaLab/countingsort.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <vector>
 
-bool counting_sort(std::vector<unsigned long long>& nums);
+bool check_memory(const std::vector<unsigned long long>&);
+void counting_sort(std::vector<unsigned long long>&,size_t,size_t);
